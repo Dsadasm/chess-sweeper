da09e2ce5a355e8ccb45ac47c48bc5abc1243cee
Anson <dsadas7000@gmail.com>
2026-01-02 21:55:30 +0800

    Home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,15 @@
+import styles from "./index.module.css";
+
 export default function HomeScreen() {
-  return <div>Home Screen</div>;
+  return (
+    <>
+      <h1 className={styles.title}>Chess Sweeper</h1>
+      <div className={styles.buttonContainer}>
+        <button className={styles.button}>RANDOM</button>
+        <button className={styles.button}>DAILY</button>
+        <button className={styles.button}>RULES</button>
+        <button className={styles.button}>LEADERBOARD</button>
+      </div>
+    </>
+  );
 }
