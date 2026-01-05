d284740e05ae479e11a1675f4714887a56a5e5ff
Notcatty137 <165467575+Notcatty137@users.noreply.github.com>
2026-01-05 20:55:09 +0800

    Add files via upload

---
+++ src/components/Sweeper.tsx
@@ -0,0 +1,121 @@
+// Contains all main parts of ChessSweeper (i.e. Board + other UI parts)
+import Board from '../components/Board';
+import React, { useState } from 'react';
+import styles from './Sweeper.module.css';
+
+// Images
+import wP from "../assets/wP.svg";
+import wR from "../assets/wR.svg";
+import wN from "../assets/wN.svg";
+import wB from "../assets/wB.svg";
+import wQ from "../assets/wQ.svg";
+import wK from "../assets/wK.svg";
+
+interface SweeperProps {
+    // Add props needed
+}
+
+export default function SweeperLayout() {
+  const [boardState, setBoardState] = useState<"reveal" | "guess">("reveal");
+  const [point, setPoint] = useState(15);
+
+  return (
+    <div className="styles.container">
+      {/* TOP HEADER (PLACEHOLDER FOR NOW) */}
+      <header className={styles.topHeader}>
+        <div className={styles.headerContent}>
+          <div className={styles.headerTitle}>
+            <h1>Chess Sweeper </h1>
+          </div>
+        </div>
+      </header>
+
+      {/* MAIN SECTION */}
+      <main className={styles.mainContent}>
+        {/* BOARD */}
+        <Board state={boardState} setPoint={setPoint} />
+        {/* SIDE BAR TO THE RIGHT (a bunch of buttons) */}
+        <aside className={styles.sidePanel}>
+            {/* ROW 1: TIMER & SCORE (NOT FUNCTIONAL YET)*/}
+            <div className={styles.buttonRow}>
+                <div className={styles.buttonGroup}>
+                <button 
+                    className={styles.button}
+                >
+                    03:00
+                </button>
+                <button 
+                    className={styles.button}
+                >
+                    15
+                </button>
+                </div>
+            </div>
+            
+            {/* ROW 2: Action Buttons */}
+            <div className={styles.buttonRow}>
+                <button className={styles.button}>
+                    (Guess)
+                </button>
+            </div>
+            
+            {/* ROW 3: Pieces Left */}
+            <div className={styles.buttonRow}>
+                <h3 className={styles.rowTitle}>Pieces Left</h3>
+                <div className={styles.buttonGroup}>
+                <button className={styles.button}>
+                    <img src={wK}></img>
+                </button>
+                <button className={styles.button}>
+                    1
+                </button>
+                <button className={styles.button}>
+                    <img src={wQ}></img>
+                </button>
+                <button className={styles.button}>
+                    1
+                </button>
+                <button className={styles.button}>
+                    <img src={wR}></img>
+                </button>
+                <button className={styles.button}>
+                    1
+                </button>
+                </div>
+            </div>
+            
+            {/* ROW 4: Special Buttons */}
+            <div className={styles.buttonRow}>
+                <div className={styles.buttonGroup}>
+                <button className={styles.button}>
+                    <img src={wB}></img>
+                </button>
+                <button className={styles.button}>
+                    1
+                </button>
+                <button className={styles.button}>
+                    <img src={wN}></img>
+                </button>
+                <button className={styles.button}>
+                    1
+                </button>
+                <button className={styles.button}>
+                    <img src={wP}></img>
+                </button>
+                <button className={styles.button}>
+                    1
+                </button>
+                </div>
+            </div>
+
+            {/* ROW 5: TOGGLE RESOLVED */}
+            <div className={styles.buttonRow}>
+                <button className={styles.button}>
+                    Toggle Resolved
+                </button>
+            </div>
+        </aside>
+      </main>
+    </div>
+  );
+}
