2c3efb9dc9bdbac28dc5f87102ac38811b5c67b9
Notcatty137 <165467575+Notcatty137@users.noreply.github.com>
2026-01-11 21:12:26 +0800

    Bugfix: Guess now ignores revealed tiles

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -71,6 +71,7 @@ export default function Board({ state, setPoint, guessChessType, cells, setCells
   // Returns points gained from the guess
   const guessPiece = (row: number, col: number): number => {
     if (guessChessType === undefined) return 0;
+    else if (cells[row][col].isRevealed) return 0;
     else if (cells[row][col].value === guessChessType) {
       // Reveal single cell on correct guess
       const newCells = [...cells];
